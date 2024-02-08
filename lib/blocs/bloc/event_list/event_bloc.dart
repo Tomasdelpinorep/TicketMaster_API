bb1a51d36fb359df394cd0d911ee079e36528a02
tomasdelpinorep <tomasdelpino28@gmail.com>
2024-02-08 14:28:17 +0100

    good start for details

--- lib/blocs/bloc/event_list/event_bloc.dart
+++ lib/blocs/bloc/event_list/event_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:ticketmaster_api/models/events_response/event.dart';
-import 'package:ticketmaster_api/repositories/event_repository.dart';
+import 'package:ticketmaster_api/repositories/eventList/event_repository.dart';
 
 part 'event_event.dart';
 part 'event_state.dart';
--- lib/repositories/eventDetails/event_details_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ticketmaster_api/models/event_details/event_details.dart';
-
-abstract class EventDetailsRepository{
-  Future<EventDetails> fetchEventDetails(int eventId);
-}
