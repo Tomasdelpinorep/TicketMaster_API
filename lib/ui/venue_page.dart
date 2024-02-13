d7a277a11db4710791c3db39621cb3fa5afe5bd9
Fran Ruiz <fjavierrule@gmail.com>
2024-02-13 14:25:38 +0100

    mejora del boton y terminar la pantalla de detalles

--- lib/ui/venue_page.dart
+++ lib/ui/venue_page.dart
@@ -46,72 +46,84 @@ Widget _venueList() {
       return ListView.builder(
           itemCount: state.venueList.length,
           itemBuilder: (context, index) {
-            return Card(
-              color: Colors.grey.shade200,
-              child: Column(
-                children: [
-                  Text(
-                    state.venueList[index].name!,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                    ),
-                  ),
-                  const Divider(),
-                  if (state.venueList[index].images != null &&
-                      state.venueList[index].images!.isNotEmpty)
-                    Image.network(
-                      state.venueList[index].images![0].url!,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    )
-                  else
-                    const Text('No image available'),
-                  Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            state.venueList[index].city!.name!,
-                            style: textStyle,
-                          ),
-                          const Text(' - ', style: textStyle),
-                          Text(state.venueList[index].country!.countryCode!,
-                              style: textStyle)
-                        ]),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => VenueDetail(
+                              id: state.venueList[index].id!,
+                            ))),
+                child: Card(
+                  elevation: 8,
+                  color: Colors.grey.shade200,
+                  child: Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 12),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => VenueDetail(
-                                          id: state.venueList[index].id!,
-                                        )));
-                          },
-                          style: ButtonStyle(
-                              minimumSize:
-                                  MaterialStateProperty.all(const Size(30, 30)),
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.lightBlue)),
-                          child: const Text(
-                            'Info',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 15),
-                          ),
+                      Text(
+                        state.venueList[index].name!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
                         ),
                       ),
+                      const Divider(),
+                      if (state.venueList[index].images != null &&
+                          state.venueList[index].images!.isNotEmpty)
+                        Image.network(
+                          state.venueList[index].images![0].url!,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        )
+                      else
+                        const Text('No image available'),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                state.venueList[index].city!.name!,
+                                style: textStyle,
+                              ),
+                              const Text(' - ', style: textStyle),
+                              Text(state.venueList[index].country!.countryCode!,
+                                  style: textStyle)
+                            ]),
+                      ),
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.end,
+                      //   children: [
+                      //     Padding(
+                      //       padding: const EdgeInsets.only(right: 12),
+                      //       child: ElevatedButton(
+                      //         onPressed: () {
+                      //           Navigator.push(
+                      //               context,
+                      //               MaterialPageRoute(
+                      //                   builder: (context) => VenueDetail(
+                      //                         id: state.venueList[index].id!,
+                      //                       )));
+                      //         },
+                      //         style: ButtonStyle(
+                      //             minimumSize: MaterialStateProperty.all(
+                      //                 const Size(30, 30)),
+                      //             backgroundColor: MaterialStateProperty.all(
+                      //                 Colors.lightBlue)),
+                      //         child: const Text(
+                      //           'Info',
+                      //           style: TextStyle(
+                      //               color: Colors.black,
+                      //               fontWeight: FontWeight.w400,
+                      //               fontSize: 15),
+                      //         ),
+                      //       ),
+                      //     ),
+                      //   ],
+                      // )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
             );
           });
